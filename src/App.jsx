9d93141abb3df57fc6ed0ec4279bad098263b5cc
DESKTOP-PJG539L\Dowen <dowen92@hotmail.co.uk>
2025-08-06 17:42:29 +0100

    Paypal donate section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from '../src/components/Header'
 import Joke from '../src/components/Joke';
-
+import Donation from '../src/components/Donation'
 function App() {
 
   return (
     <>
       <Header />
       <Joke />
+      <Donation/>
     </>
   )
 }
